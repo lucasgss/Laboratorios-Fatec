51fe52f9b483e1cd52ec36fcbba40fa2eb7f3c81
Lucas Soares <lucas_ssoares@live.com>
2019-01-02 21:05:43 -0200

    Adicionado metodos de busca, paginação e contagem de registros.

---
+++ app/static/js/base.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.findList').DataTable({
+        "language": {
+            "url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/Portuguese-Brasil.json"
+        }
+    });
+});
